58ee0818f931f00d0a594706a29e818b79481104
TheBuddyzOfficial <126583984+TheBuddiesOfficial@users.noreply.github.com>
2025-07-02 01:14:51 +0500

    Updated FloatingParticles.tsx

---
+++ src/components/FloatingParticles.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+interface Particle {
+  id: number;
+  x: number;
+  y: number;
+  size: number;
+  color: string;
+  duration: number;
+  delay: number;
+}
+
+export const FloatingParticles: React.FC = () => {
+ null
+  );
+};
